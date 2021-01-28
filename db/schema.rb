c4dd96044d374486dab38fed00646fe79324f000
MyPC <hopu38@yahoo.co.jp>
2021-01-29 02:24:43 +0900

    Post User conect

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_144805) do
+ActiveRecord::Schema.define(version: 2021_01_28_145508) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "image_name"
+    t.string "password"
   end
 
 end
